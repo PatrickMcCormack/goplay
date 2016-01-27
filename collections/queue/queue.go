d0c9069c23b3c85b54e6ec66267a1fa11dc8d935
Patrick McCormack <pnmccormack@yahoo.com>
2016-01-26 17:35:12 -0800

    Finish initial implementation

--- collections/queue/queue.go
+++ collections/queue/queue.go
@@ -9,7 +9,7 @@ import (
 
 type Element struct {
   value interface{}
-  next *Element // points to the tail
+  next *Element
 }
 
 type Queue struct {
@@ -19,11 +19,24 @@ type Queue struct {
 }
 
 func (q *Queue) push(v interface{}) {
-  q.Tail := &Element{v, q.tail}
+  oldLast := q.tail
+  q.tail = &Element{v, nil}
+  if q.queueSize == 0 {
+    q.head = q.tail
+  } else {
+    oldLast.next = q.tail
+  }
+  q.queueSize++
 }
 
 func (q *Queue) pop() interface{} {
-  return "xx"
+  if q.head == nil { // case where there are no elements in the queue
+    return nil
+  }
+  value := q.head.value
+  q.head = q.head.next
+  q.queueSize--
+  return value
 }
 
 func (q *Queue) size() int {
@@ -32,6 +45,19 @@ func (q *Queue) size() int {
 
 func main() {
   var queue Queue
-  queue.push("xx")
-  fmt.Printf("Hello")
+  queue.push(1)
+  queue.push(2)
+  queue.push(3)
+  fmt.Printf("size  = %d\n", queue.size())
+  fmt.Printf("value = %d\n", queue.pop())
+  fmt.Printf("size  = %d\n", queue.size())
+  fmt.Printf("value = %d\n", queue.pop())
+  fmt.Printf("size  = %d\n", queue.size())
+  fmt.Printf("value = %d\n", queue.pop())
+  fmt.Printf("size  = %d\n", queue.size())
+  if queue.pop() == nil {
+    fmt.Println("queue is empty")
+    fmt.Printf("queue.head == %v\n", queue.head)
+    fmt.Printf("queue.tail == %v\n", queue.tail)
+  }
 }
