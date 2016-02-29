9c2580497c9d44d7170114a7e680a6da82580c34
Patrick McCormack <pnmccormack@yahoo.com>
2016-02-28 22:37:50 -0800

    Fixing deadlock

--- collections/heap.go
+++ collections/heap.go
@@ -2,7 +2,9 @@ package collections
 
 import "sync"
 
-// todo: delete a node, lots of testing
+// todo:
+// 1. delete a node, lots of testing
+// 2. MT safety needs more work
 
 // Heap respresent a heap data structure. Heap data structures are often used
 // as priority queues.
@@ -24,8 +26,8 @@ func (aHeap *Heap) Initialize(initialSize int, c Comparator) {
 
 // Calculate the array offset for the parent of a given node
 func (aHeap *Heap) parent(index int) int {
-	aHeap.RLock()
-	defer aHeap.RUnlock()
+	//	aHeap.RLock()
+	//	defer aHeap.RUnlock()
 	if index == 0 {
 		return 0
 	}
@@ -33,14 +35,14 @@ func (aHeap *Heap) parent(index int) int {
 }
 
 func (aHeap *Heap) leftChildOf(index int) int {
-	aHeap.RLock()
-	defer aHeap.RUnlock()
+	//	aHeap.RLock()
+	//	defer aHeap.RUnlock()
 	return 2*index + 1
 }
 
 func (aHeap *Heap) rightChildOf(index int) int {
-	aHeap.RLock()
-	defer aHeap.RUnlock()
+	//	aHeap.RLock()
+	//	defer aHeap.RUnlock()
 	return 2*index + 2
 }
 
