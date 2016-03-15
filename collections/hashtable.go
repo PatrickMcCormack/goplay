cc5c9df0a14411809b19d6b6afbe101df6540caf
Patrick McCormack <pnmccormack@yahoo.com>
2016-03-15 16:48:42 -0700

    First working version

--- collections/hashtable.go
+++ collections/hashtable.go
@@ -1,55 +1,144 @@
 package collections
 
-// Very rough WIP, nothing to see here, move on ...
+// hashtable - an implementation of a hashtable/dictionary
+// PatrickMcCormack
 
 import (
+	"bytes"
+	"encoding/gob"
+	"fmt"
 	"hash/fnv"
+	"sync"
 )
 
-type hashElement struct {
-	name  string
-	value string
+type hashBucket struct {
+	name  interface{}
+	value interface{}
 }
 
+// HashTable is a structure which is used to implement a hash table.
+// The hashtable must be initialized before use either by calling the
+// Initialize method or by initlization of the buckets field on creation.
 type HashTable struct {
-	htable []*hashElement
-	size   int
+	buckets      []*LinkedList
+	numEntries   int
+	sync.RWMutex // composite object
 }
 
-func (h *HashTable) Initialize() {
-	h.htable = make([]*hashElement, 100)
-}
+var (
+	// DefaultNumBuckets is the number of buckets created
+	// if <=0 is passed to Initalize
+	DefaultNumBuckets = 100
+)
 
-// Insert
-func (h *HashTable) Insert(name string, value string) {
-	hasher := fnv.New64a()
-	hasher.Write([]byte(name))
-	hv := hasher.Sum64()
-	index := hv % 100
-	h.htable[index] = &hashElement{name, value}
-	h.size++
-}
-
-// Find
-func (h *HashTable) Find(name string) (bool, string) {
-	hasher := fnv.New64a()
-	hasher.Write([]byte(name))
-	hv := hasher.Sum64()
-	index := hv % 100
-	var rvalue string
-	var status bool
-	if h.htable[index] != nil {
-		rvalue = h.htable[index].value
-		status = true
+// Initialize a HashTable, to create a HashTable with a non-default number
+// of buckets you must pass a non-zero positive integer to Intialize.
+// The Initialize method is not idempotent.
+func (h *HashTable) Initialize(size int) {
+	h.Lock()
+	defer h.Unlock()
+	if size <= 0 {
+		h.buckets = make([]*LinkedList, DefaultNumBuckets)
 	} else {
-		rvalue = ""
-		status = false
+		h.buckets = make([]*LinkedList, size)
 	}
-	return status, rvalue
 }
 
-// Delete
-func (h *HashTable) Delete(name string) (bool, string) {
-	h.size--
-	return false, ""
+func (h *HashTable) calcHash(name interface{}) (int, error) {
+	// This code converts a interface{} to a byte array
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+	err := enc.Encode(name)
+	hash := 0
+	// calculate the hash
+	if err == nil {
+		hasher := fnv.New32a()
+		hasher.Write(buf.Bytes())
+		hv := hasher.Sum32()
+		hash = int(hv % uint32(len(h.buckets)))
+	}
+	fmt.Printf("the hash for %v is %v, err is %v\n", name, hash, err)
+	return hash, err
+}
+
+// Insert a name/value into the hash table.
+func (h *HashTable) Insert(name interface{}, value interface{}) error {
+	h.Lock()
+	defer h.Unlock()
+	index, err := h.calcHash(name)
+	if err != nil {
+		return err
+	}
+	bucketList := h.buckets[index]
+	if bucketList == nil {
+		newBucketList := &LinkedList{}
+		bucket := &hashBucket{name: name, value: value}
+		newBucketList.Insert(bucket)
+		h.buckets[index] = newBucketList
+	} else {
+		var bucket *hashBucket
+		iterator := bucketList.Iterator()
+		for bucket := iterator(); bucket != nil; bucket = iterator() {
+			if bucket.(*hashBucket).name == name {
+				break
+			}
+		}
+		if bucket != nil {
+			bucketList.Delete(bucket)
+			newBucket := &hashBucket{name: name, value: value}
+			bucketList.Insert(newBucket)
+		} else {
+			newBucket := &hashBucket{name: name, value: value}
+			bucketList.Insert(newBucket)
+		}
+	}
+	return err
+}
+
+// Find a value in the hash table giving a key.
+func (h *HashTable) Find(name interface{}) (interface{}, error) {
+	h.RLock()
+	defer h.RUnlock()
+	index, err := h.calcHash(name)
+	if err != nil {
+		return nil, err
+	}
+	var rvalue interface{}
+	rvalue = nil
+	if h.buckets[index] != nil {
+		bucketList := h.buckets[index]
+		iterator := bucketList.Iterator()
+		for bucket := iterator(); bucket != nil; bucket = iterator() {
+			if bucket.(*hashBucket).name == name {
+				rvalue = bucket.(*hashBucket).value
+				break
+			}
+		}
+	}
+	return rvalue, err
+}
+
+// Delete an entry in the hash table, returns false if the entry to be deleted
+// does not exist or there is is an internal error in the delete method.
+func (h *HashTable) Delete(name interface{}) error {
+	h.Lock()
+	defer h.Unlock()
+	index, err := h.calcHash(name)
+	if err != nil {
+		return err
+	}
+	bucketList := h.buckets[index]
+	if bucketList != nil {
+		iterator := bucketList.Iterator()
+		bucket := iterator()
+		for ; bucket != nil; bucket = iterator() {
+			if bucket.(*hashBucket).name == name {
+				break
+			}
+		}
+		if bucket != nil {
+			bucketList.Delete(bucket)
+		}
+	}
+	return err
 }
--- collections/hastable_test.go
+++ collections/hastable_test.go
@@ -2,8 +2,8 @@ package collections
 
 import "testing"
 
-var nameTestData=[]string{"Moby"}
-var valueTestData=[]string{"Extreme Ways"}
+var nameTestData = []string{"Moby", "Radiohead"}
+var valueTestData = []string{"Extreme Ways", "The Bends"}
 
 func TestHashtable(t *testing.T) {
 
@@ -12,20 +12,55 @@ func TestHashtable(t *testing.T) {
 	// ---------------------------------------------------------------------
 	t.Log("==Start: Creating HT, insert an element, retrieve & check element")
 
-		var h HashTable
-		h.Initialize()
-		h.Insert(nameTestData[0], valueTestData[0])
-		_, value := h.Find(nameTestData[0])
-		t.Logf("Got %v, expecting %v", value, valueTestData[0])
-		if value !=  valueTestData[0] {
-			t.Log("Hashtable returning wrong value")
-			t.Fail()
-		}
+	var h HashTable
+	h.Initialize(0)
+
+	h.Insert(nameTestData[0], valueTestData[0])
+	value, err := h.Find(nameTestData[0])
+	t.Logf("Got %v, expecting %v\n", value, valueTestData[0])
+	if value != valueTestData[0] {
+		t.Log("Hashtable returning wrong value")
+		t.Fail()
+	}
+	if err != nil {
+		t.Logf("Find returning error - %v\n", err)
+		t.Fail()
+	}
+
+	h.Insert(nameTestData[1], valueTestData[1])
+	value, err = h.Find(nameTestData[1])
+	t.Logf("Got %v, expecting %v", value, valueTestData[1])
+	if value != valueTestData[1] {
+		t.Log("Hashtable returning wrong value")
+		t.Fail()
+	}
+	if err != nil {
+		t.Logf("Find returning error - %v\n", err)
+		t.Fail()
+	}
+
+	value, err = h.Find("Won't find me")
+	if value != nil {
+		t.Log("Hashtable found an impossible record")
+		t.Fail()
+	}
+	if err != nil {
+		t.Logf("Find returning error - %v\n", err)
+		t.Fail()
+	}
+
+	// Test Delete
+	h.Delete(nameTestData[0])
+	value, err = h.Find(nameTestData[0])
+	t.Logf("Got %v, nil", value)
+	if err != nil {
+		t.Logf("Delete returning error - %v\n", err)
+		t.Fail()
+	}
 
 	t.Log("==Completed: Creating HT, insert an element, retrieve & check element")
 
 	// ---------------------------------------------------------------------
 	t.Log("Done testing Hashtable functionality")
-
 
 }
